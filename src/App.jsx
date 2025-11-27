4f139c915186e4430438b158675f7ca45121754a
Karar Hussain <kararhussain03212@gmail.com>
2025-11-28 03:07:57 +0500

    your commit message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl text-red-700'>Read me</div>
+    </>
+  )
+}
+
+export default App
